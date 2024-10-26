820b6573a89bb16b87eb922c9886d0fb55cea7c9
Jack Gibbs <jackgibbs66@icloud.com>
2024-10-26 12:38:25 +0100

    Commit initial project structure.

---
+++ src/main/java/com/splitnest/SplitnestApplication.java
@@ -0,0 +1,13 @@
+package com.splitnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitnest/SplitnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
